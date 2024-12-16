35fffdce5531115482ea5d231656d74619d1d6c3
NNG-52 <108219544+NNG-52@users.noreply.github.com>
2024-12-16 11:46:34 +1100

    almost done with fake path

--- Collision_management/fakepath.py
+++ Collision_management/fakepath.py
@@ -1,12 +1,23 @@
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
 
-
+# number of points
+point_num = 100
+# make a line
+x = list(range(1,point_num))
+m = -1
+c = 1
+y = m*x + c
 
 fakepath = Path()
 fakepath.header =
-for i in 100:
+for i in point_num:
     stampedposes = PoseStamped()
-    stampedposes.point
-    stampedposes.orientation
+    stampedposes.point.x = x[i] 
+    stampedposes.point.y = y[i]
+    stampedposes.point.z = 0.0
+    stampedposes.orientation.x = 0.0
+    stampedposes.orientation.y = 0.0
+    stampedposes.orientation.z = 0.0
+    stampedposes.orientation.w = 1.0
     fakepath.poses.push(stampedposes)
