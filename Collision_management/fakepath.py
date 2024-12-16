117f4c8f23fa05594408342d2ab6fedf69931949
NNG-52 <108219544+NNG-52@users.noreply.github.com>
2024-12-16 11:02:39 +1100

    making a fake path to test navigation

---
+++ Collision_management/fakepath.py
@@ -0,0 +1,12 @@
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+
+
+fakepath = Path()
+fakepath.header =
+for i in 100:
+    stampedposes = PoseStamped()
+    stampedposes.point
+    stampedposes.orientation
+    fakepath.poses.push(stampedposes)
